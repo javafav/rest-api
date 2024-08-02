c266231055a9e2f3cebbf234a29afc11490f1789
javafav <130149164+javafav@users.noreply.github.com>
2024-08-02 23:31:11 +0500

    Add Location Repository and related tests, services, and controllers

---
+++ WeatherApiProject/WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationRepositoryTests.java
@@ -0,0 +1,38 @@
+package com.skyapi.weatherforecast.location;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.skyapi.weatherforecast.common.Location;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class LocationRepositoryTests {
+
+	@Autowired
+	private LocationRepository repo;
+	
+	
+	@Test
+	public void testAddNewLocation() {
+		
+		Location location = new Location();
+		location.setCode("UCH");
+		location.setCityName("Uch Sharif");
+		location.setReigonName("Bahwalpur");
+		location.setCounrtyCode("PK");
+		location.setCountryName("Pakistan");
+		
+		Location savedLoaction = repo.save(location);
+		
+		assertThat(savedLoaction.getCode().equals("UCH"));
+		
+	}
+}
